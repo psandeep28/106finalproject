94efa2d81c1bc05ad0a806e86b1a8f0eeb5879c5
Parth Sandeep <psandeep@ucsd.edu>
2025-05-24 18:41:44 -0700

    circle test

--- script.js
+++ script.js
@@ -10,3 +10,25 @@ scroller
     console.log("Entered step", response.index);
     // You’ll add D3 animations here based on index
   });
+
+  // Select the SVG and draw one circle
+const svg = d3.select("#chart");
+
+svg.append("circle")
+  .attr("cx", 300)
+  .attr("cy", 200)
+  .attr("r", 50)
+  .attr("fill", "#69b3a2");
+
+  scroller.onStepEnter((response) => {
+    const index = response.index;
+  
+    svg.select("circle")
+      .transition()
+      .duration(500)
+      .attr("fill", index === 0 ? "#69b3a2" :
+                      index === 1 ? "#f28e2b" :
+                      index === 2 ? "#e15759" :
+                      "#76b7b2");
+  });
+  
