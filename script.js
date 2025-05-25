bb5a7772dfc783644a5d785776ebead2dd31d018
Parth Sandeep <psandeep@ucsd.edu>
2025-05-24 19:34:26 -0700

    pls

--- script.js
+++ script.js
@@ -1,31 +1,28 @@
-// Initialize Scrollama
+// Initialize scrollama
 const scroller = scrollama();
 
-// Select the SVG and draw one circle
+// Draw a basic circle in the SVG
 const svg = d3.select("#chart");
-
 svg.append("circle")
   .attr("cx", 300)
   .attr("cy", 200)
   .attr("r", 50)
   .attr("fill", "#69b3a2");
 
-// Set up Scrollama + single onStepEnter handler
+// Scrollama event setup
 scroller
   .setup({
     step: ".step",
     offset: 0.5,
-    debug: true
+    debug: false
   })
-  .onStepEnter((response) => {
+  .onStepEnter(response => {
     const index = response.index;
-    console.log("Entered step", index);
 
-    // Change color of the circle on scroll
     svg.select("circle")
       .transition()
       .duration(500)
-      .attr("fill", 
+      .attr("fill",
         index === 0 ? "#69b3a2" :
         index === 1 ? "#f28e2b" :
         index === 2 ? "#e15759" :
