eebb7f86b4056a2ab134e6323e03b100bae82f63
Martin Hawks <martinhawks8@gmail.com>
2025-06-01 17:59:33 -0700

    Made interactive chart work

--- user-ineract.js
+++ user-ineract.js
@@ -1,7 +1,7 @@
 import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
 import { createRadarChart } from './script.js';
 
-let testData = {
+let userData = {
   name: 'User',
   depressionRate: 0,
   avgSleep: '6 hours',
@@ -17,10 +17,28 @@ let testData = {
   ],
 };
 
+
 function addSliderValues() {
-  let sliders = Array.from(document.getElementsByClassName('userin'));
+  let inputs = Array.from(document.getElementsByClassName('userin'));
+  for (let userInput of inputs) {
+    let slider = userInput.getElementsByClassName('user-input')[0];
+    slider.addEventListener('input', updateSliderValues);
+  }
+  updateSliderValues();
 }
 
+function updateSliderValues() {
+  let inputs = Array.from(document.getElementsByClassName('slider'));
+  for (let userInput of inputs) {
+    let slider = userInput.getElementsByClassName('user-input')[0];
+    let label = userInput.getElementsByClassName('value')[0];
+    let dataName = slider.name;
+    let dataPoint = userData.detailData.filter((f) => f.factor === dataName)[0];
+    dataPoint.value = slider.value;
+    label.innerHTML = slider.value;
+  }
+  createRadarChart(userData, '#user-chart');
+}
 
-createRadarChart(testData, '#user-chart');
+createRadarChart(userData, '#user-chart');
 addSliderValues();
