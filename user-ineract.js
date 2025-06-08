6f2a1f8bf39f0c04564f05f4e1e0bec8a5e07e55
Martin Hawks <martinhawks8@gmail.com>
2025-06-08 13:56:38 -0700

    Working model and initial integration

--- user-ineract.js
+++ user-ineract.js
@@ -1,6 +1,8 @@
 import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
 import { createRadarChart } from './script.js';
 
+const model = await tf.loadGraphModel('./javaScriptModel/model.json');
+
 let userData = {
   name: 'User',
   depressionRate: 0,
@@ -8,6 +10,7 @@ let userData = {
   stressLevel: 0.7,
   sleepScore: 65,
   insights: "this is a test user",
+  hours: 0,
   detailData: [
     { factor: 'Academic Pressure', value: 80 },
     { factor: 'Financial Stress', value: 80 },
@@ -35,19 +38,57 @@ function updateSliderValues() {
     let slider = userInput.getElementsByClassName('user-input')[0];
     let label = userInput.getElementsByClassName('value')[0];
     let dataName = slider.name;
-    let dataPoint = userData.detailData.filter((f) => f.factor === dataName)[0];
+    let sliderVal = Number(slider.value);
+    let dataPoint = {};
     if (dataName === 'Academic Pressure') {
       bufferAcademic = Number(slider.value);
     } else if (dataName === "Financial Stress") {
       bufferFin = Number(slider.value);
     }
-    dataPoint.value = Number(slider.value);
+    if (dataName === "Work/Study Hours") {
+      userData.hours = sliderVal
+    } else {
+      dataPoint = userData.detailData.filter((f) => f.factor === dataName)[0];
+      dataPoint.value = sliderVal;
+    }
     label.innerHTML = slider.value;
   }
+  let hoursWorked = document.getElementById('hrs').value;
   createRadarChart(userData, '#user-chart');
   userData.stressLevel = (bufferAcademic + bufferFin) / 2;
-  console.log(userData.stressLevel);
+}
+
+async function predictUser() {
+  let sleepDur = Number(document.getElementById('sleep-amnt').value);
+  let workPress = (100 - userData.detailData.filter((f) =>
+    f.factor === 'Work-Life Balance')[0].value) * (5 / 100);
+  // Scale the above value to fit the 0-5 format that the training data has
+  let finStress = (userData.detailData.filter((f) =>
+    f.factor === 'Financial Stress')[0].value * (4 / 100)) + 1;
+  // Original data also has 0-5 scale for this, but starts at 1
+  // (I think the training data is ordinal, and not ratio, but it should still work
+  // since it is a nn accepting floats)
+  let acPress = (100 - userData.detailData.filter((f) =>
+    f.factor === 'Academic Pressure')[0].value) * (5 / 100);
+  let hrs = userData.hours;
+  let modelInput = Array(workPress, sleepDur, finStress, hrs, acPress);
+  console.log(modelInput);
+  modelInput = tf.tensor2d([modelInput], [1, 5])
+  let userScore = model.predict(modelInput);
+  userScore = await userScore.data();
+  userData.score = userScore;
+  showUserRisk(userData);
+}
+
+function showUserRisk(data) {
+  let risk = Math.round(data.score * 100);
+  let riskBlock = document.getElementById('user-risk')
+  riskBlock.classList.remove('hidden')
+  riskBlock.children[1].innerHTML = risk;
+  console.log(riskBlock);
 }
 
 createRadarChart(userData, '#user-chart');
 addSliderValues();
+
+document.getElementById('run-model').addEventListener('click', predictUser)
